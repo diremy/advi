586ca8e7fd815570faf5881feabdd1e8efa9927d
weis <>
2001-12-08 14:02:46 +0000

    Code review.

--- draw_image.ml
+++ draw_image.ml
@@ -9,19 +9,21 @@ let verbose_image_access = Misc.option_flag false
     "--verbose_image_access"
     "\tChange the cursor while image loadings";;
 
-let images_camlimage = Hashtbl.create 107
-let images_graphics = Hashtbl.create 107
+let images_camlimage = Hashtbl.create 107;;
+let images_graphics = Hashtbl.create 107;;
 
-let cache_dir = ".advi"
+let cache_dir = ".advi";;
 
-type ratiopts = 
-  | ScaleX        (* scale x coords, but keep original Ratio *)
-  | ScaleY        (* scale y coords, but keep original Ratio *)
-  | Original      (* respect original size                   *)
-  | FreeScale     (* scale to fit requested area             *)
+type ratiopts =
+   | ScaleX        (* scale x coords, but keep original Ratio *)
+   | ScaleY        (* scale y coords, but keep original Ratio *)
+   | Original      (* respect original size                   *)
+   | FreeScale     (* scale to fit requested area             *)
 ;;
 
-let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
+let white_rgb = {r = 255; g = 255; b = 255};;
+
+let f file whitetransp alpha blend ratiopts (w, h) x0 y0 =
   let file = Search.true_file_name [] file in
   let cache_name =   (* The computed cache name *)
     let file' = String.copy file in
@@ -37,6 +39,7 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
 
   (* Load an image from the cache *)
   let load () =
+    prerr_endline "Loading from cache";
     (* we have no trivial image format for rgba32! *)
     if whitetransp then begin
       let ic = open_in_bin cache_name in
@@ -44,7 +47,7 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
       let height = input_value ic in
       let data = input_value ic in
       close_in ic;
-      Rgba32 (Rgba32.create_with width height data) 
+      Rgba32 (Rgba32.create_with width height data)
     end else begin
       let ic = open_in_bin cache_name in
       let width = input_value ic in
@@ -55,11 +58,10 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
     end
   in
 
-
   (* Save an image into the cache *)
   let save = function
     | Rgba32 image ->
-	begin try 
+	begin try
 	  let oc = open_out_bin cache_name in
 	  output_value oc image.Rgba32.width;
 	  output_value oc image.Rgba32.height;
@@ -67,22 +69,22 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
 	  close_out oc
 	with _ -> () end
     | Rgb24 image ->
-	begin try 
+	begin try
 	  let oc = open_out_bin cache_name in
 	  output_value oc image.Rgb24.width;
 	  output_value oc image.Rgb24.height;
 	  output_value oc (Bitmap.dump image.Rgb24.data);
 	  close_out oc
 	with _ -> () end
-    | _ -> () 
+    | _ -> ()
   in
 
   let image = (* here one builds the image structure *)
     try
-      if (Unix.stat file).Unix.st_mtime >= 
+      if (Unix.stat file).Unix.st_mtime >=
 	   (Unix.stat cache_name).Unix.st_mtime
       then raise Exit;
-      try 
+      try
 	Hashtbl.find images_camlimage cache_name
       with Not_found ->
       	(* Use the cache file *)
@@ -98,24 +100,22 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
 	  (* we skip anti-aliasing for now ... *)
           try
             Image.load file []
-          with Image.Wrong_file_type -> failwith ("Unsupported graphic format in: "^file)
+          with Image.Wrong_file_type ->
+            failwith ("Unsupported graphic format in: " ^ file)
       	in
 	let image = match image with
 	| Rgb24 i ->
-  	    let width = i.Rgb24.width 
+  	    let width = i.Rgb24.width
   	    and height = i.Rgb24.height
   	    in
 	    if whitetransp then begin
   	      let rgba = Rgba32.create width height in
   	      for y = 0 to height - 1 do
-  	      for x = 0 to width - 1 do 
-  		let rgb = Rgb24.unsafe_get i x y in
-  		let a = 
-  		  if whitetransp && rgb = {r=255;g=255;b=255} then 0
-  		  else 255
-  		in
-  		Rgba32.unsafe_set rgba x y { color=rgb; alpha=a }
-  	      done
+               for x = 0 to width - 1 do
+                 let rgb = Rgb24.unsafe_get i x y in
+                 let a = if whitetransp && rgb = white_rgb then 0 else 255 in
+                 Rgba32.unsafe_set rgba x y { color = rgb; alpha = a }
+               done
   	      done;
   	      Rgba32 (Rgba32.resize None rgba w h)
 	    end else begin
@@ -135,10 +135,10 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
 
   match image with
   | Rgb24 _ when alpha = 1.0 && blend = None ->
-      (* optimized *) 
+      (* optimized *)
       let image_graphics =
 	try
-	  Hashtbl.find images_graphics cache_name 
+	  Hashtbl.find images_graphics cache_name
 	with
 	| Not_found ->
 	    let im = Graphic_image.of_image image in
@@ -152,14 +152,14 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
       let get_src_alpha =
 	match image with
 	| Rgb24 image -> fun x y -> Rgb24.unsafe_get image x y, alpha
-	| Rgba32 image -> 
+	| Rgba32 image ->
 	    fun x y ->
 	      let {color= src; alpha= a} = Rgba32.unsafe_get image x y in
 	      src, a * alpha / 255
 	| _ -> assert false
       in
 
-      let blend = 
+      let blend =
 	match blend with
 	| Some b -> b
 	| None -> fun dst src -> src
@@ -169,14 +169,14 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
       let coloropt a =
     	match a with
     	| 0 -> fun dst src -> dst
-    	| 255 -> 
-	    fun dst src -> 
+    	| 255 ->
+	    fun dst src ->
 	      {r= blend dst.r src.r;
 	       g= blend dst.g src.g;
 	       b= blend dst.b src.b }
-    	| _ -> 
+    	| _ ->
 	    let a' = 255 - a in
-	    fun dst src -> 
+	    fun dst src ->
 	      {r= (blend dst.r src.r * a + dst.r * a') / 255;
 	       g= (blend dst.g src.g * a + dst.g * a') / 255;
 	       b= (blend dst.b src.b * a + dst.b * a') / 255 }
@@ -191,21 +191,23 @@ let f file whitetransp alpha blend ratiopts (w,h) x0 y0 =
       Graphic_image.draw_image (Rgb24 org) x0 y0
 *)
       ()
-  | _ -> assert false
+  | _ -> assert false;;
 
 let clean_cache () =
-  (* erase the files match with "cache-*" *)
+  (* erase the files whose name matches with "cache-*" *)
   let prefix = Filename.concat cache_dir "cache-" in
   let suffix = "" in
+  let suff_len = String.length suffix in
+  let pref_len = String.length prefix in
   let dh = Unix.opendir "." in
   try while true do
     let file = Unix.readdir dh in
-    if String.length file > 10 &&
-       String.sub file 0 (String.length prefix) = prefix &&
-       String.sub file (String.length file - String.length suffix)
-	               (String.length suffix) = suffix then begin
-			 Unix.unlink file
-		       end
-  done with End_of_file -> 
-    Unix.closedir dh
+    let file_len = String.length file in
+    if file_len > 10 &&
+       String.sub file 0 pref_len = prefix &&
+       String.sub file (file_len - suff_len) suff_len = suffix
+    then Unix.unlink file
+  done with End_of_file ->
+    Unix.closedir dh;;
+
 
