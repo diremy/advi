37aa4a4c037962d58ff4dbb99c15a933b7028a96
weis <>
2003-09-29 09:26:04 +0000

    Code review.

--- test/adviclock.ml
+++ test/adviclock.ml
@@ -9,62 +9,62 @@
 (*                                                                     *)
 (***********************************************************************)
 
-open Camltk
+open Camltk;;
 
-(** Diameter of the circle. *)
-let diametre = ref 200
+(** Diameter of the clock. *)
+let diametre = ref 200;;
 
 (** Background color. *)
-let background_color = ref "White"
+let background_color = ref "White";;
 
-(** Total major time for 360 degres (in miliseconds) *)
-let base_major = ref 3600000.0
+(** Total major time for 360 degrees (in miliseconds) *)
+let base_major = ref 3600000.0;;
 
-(** Total minor time for 360 degres (in miliseconds) *)
-let base_minor = ref 60000.0
+(** Total minor time for 360 degrees (in miliseconds) *)
+let base_minor = ref 60000.0;;
 
 (** Color of the major hand. *)
-let color_major = ref "FireBrick"
+let color_major = ref "FireBrick";;
 
 (** Color of the minor hand. *)
-let color_minor = ref "Blue"
+let color_minor = ref "Blue";;
 
 (** Width of the major hand. *)
-let width_major = ref 2
+let width_major = ref 2;;
 
 (** Width of the major hand. *)
-let width_minor = ref 1
+let width_minor = ref 1;;
 
 (** Width of the circle. *)
-let width_circle = ref 0
+let width_circle = ref 0;;
 
 (** Refresh frequency (in milliseconds). *)
-let frequency = ref 1000.0
+let frequency = ref 1000.0;;
 
 (** Reverse or not. *)
-let reverse = ref false
+let reverse = ref false;;
 
 (** Total time, in theory :-), in milliseconds. *)
-let total_time = ref 1800000.0
+let total_time = ref 1800000.0;;
 
 (** Color for the authorized time interval. *)
-let color_ok = ref "PaleGreen2"
+let color_ok = ref "PaleGreen2";;
 
 (** Color for the done time interval. *)
-let color_done = ref "Turquoise3"
+let color_done = ref "Turquoise3";;
 
 (** Color for the 'you're late' time interval. *)
-let color_overtime = ref "Red"
+let color_overtime = ref "Red";;
 
 (** Predefined times (in milliseconds). *)
-let times = ref ([] : float list)
+let times = ref ([] : float list);;
 
 (** The filename of the interface file. *)
-let filename = ref (None : string option)
+let filename = ref (None : string option);;
 
-let pi = 4.0 *. atan 1.0
-let base_x = 2
-let base_y = 2
+let pi = 4.0 *. atan 1.0;;
+let base_x = 2;;
+let base_y = 2;;
 
 module Args = struct
 
@@ -103,22 +103,24 @@ module Args = struct
       "<color>  Set the color name used for the sectors not done yet";
     "-over", Arg.String (fun s -> color_overtime := s),
       "<color>  Set the color name used for the overtime";
-  ] 
+  ]
 
-  let parse () = Arg.parse 
+  let parse () = Arg.parse
       (keywords @ options)
-      (fun s -> 
-        try times := !times @ [ float_of_string s *. 1000.0] 
-        with Invalid_argument err -> 
-          prerr_endline ("Invalid_argument " ^ err ^ " : " ^ s)
+      (fun s ->
+        try times := !times @ [ float_of_string s *. 1000.0] with
+        | Invalid_argument err ->
+            prerr_endline
+              (Printf.sprintf "Invalid_argument %s : %s" err s)
       )
-      ("usage : " ^ Sys.argv.(0) ^
-       " [options] t1 t2 ... (in seconds)\nOptions are:")
-end
-  
+      (Printf.sprintf
+         "usage : %s [options] t1 t2 ... (in seconds)\nOptions are:"
+         Sys.argv.(0))
+end;;
+
 let opencamltk () =
   Args.parse ();
-  opentk ()
+  opentk ();;
 
 type clock = {
     widget : Widget.widget;
@@ -126,42 +128,42 @@ type clock = {
     mutable minor : tagOrId;
     mutable major_elapsed : float;
     mutable minor_elapsed : float;
-  } 
+  };;
 
 let compute_coord rayon base_time time  =
   let angle = (time /. base_time) *. (2. *. pi) in
   let x = cos angle *. float_of_int rayon in
   let y = sin angle *. float_of_int rayon in
-  (int_of_float y, - (int_of_float x))
+  (int_of_float y, - (int_of_float x));;
 
 let options_circle () =
-  [Width (Pixels !width_circle)]
+  [Width (Pixels !width_circle)];;
 let options_major () =
-  [Width (Pixels !width_major); FillColor (NamedColor !color_major)]
+  [Width (Pixels !width_major); FillColor (NamedColor !color_major)];;
 let options_minor () =
-  [Width (Pixels !width_minor); FillColor (NamedColor !color_minor)]
+  [Width (Pixels !width_minor); FillColor (NamedColor !color_minor)];;
 
-let draw_major c = 
+let draw_major c =
   Canvas.delete c.widget [c.major];
   let d = !diametre in
   let r = d / 2 in
   let (x,y) = compute_coord r !base_major c.major_elapsed in
   c.major <-
     Canvas.create_line c.widget
-      [ Pixels (base_x + r); Pixels (base_y + r); 
+      [ Pixels (base_x + r); Pixels (base_y + r);
 	Pixels (base_x + r + x); Pixels(base_y + r + y)]
-      (options_major ())
+      (options_major ());;
 
-let draw_minor c = 
+let draw_minor c =
   Canvas.delete c.widget [c.minor];
   let d = !diametre in
   let r = d / 2 in
   let (x,y) = compute_coord r !base_minor c.minor_elapsed in
   c.minor <-
     Canvas.create_line c.widget
-      [ Pixels (base_x + r); Pixels (base_y + r); 
+      [ Pixels (base_x + r); Pixels (base_y + r);
 	Pixels (r + x); Pixels(r + y)]
-      (options_minor ())
+      (options_minor ());;
 
 
 let create_clock () =
@@ -173,7 +175,7 @@ let create_clock () =
   pack [w] [Expand true; Fill Fill_Both ];
   if !width_circle > 0 then ignore (
     Canvas.create_oval w
-      (Pixels base_x) (Pixels base_y) 
+      (Pixels base_x) (Pixels base_y)
       (Pixels (base_x + d)) (Pixels (base_y + d))
       (options_circle ()));
   let clock =
@@ -181,11 +183,11 @@ let create_clock () =
       widget = w;
       major =
         Canvas.create_line w
-          [ Pixels (base_x + r); Pixels (base_y + r); 
+          [ Pixels (base_x + r); Pixels (base_y + r);
 	    Pixels (base_x + r); Pixels base_y] (options_major ());
       minor =
         Canvas.create_line w
-          [ Pixels (base_x + r); Pixels (base_y + r); 
+          [ Pixels (base_x + r); Pixels (base_y + r);
             Pixels (base_x + r); Pixels base_y] (options_minor ());
       major_elapsed = (if !reverse then !total_time else 0.0);
       minor_elapsed = 0.0;
@@ -193,7 +195,7 @@ let create_clock () =
   in
   draw_major clock;
   draw_minor clock;
-  clock
+  clock;;
 
 
 let draw_overtime_arc c old_major =
@@ -201,7 +203,7 @@ let draw_overtime_arc c old_major =
   let inf = if !reverse then 0.0 else -. !total_time in
   let angle_base = 360.0 in
   let angle_inf = (inf /. !base_major) *. angle_base +. 90.0 in
-  let angle_sup = 
+  let angle_sup =
     if !reverse then
       -. ((c.major_elapsed /. !base_major) *. angle_base)
     else
@@ -212,13 +214,13 @@ let draw_overtime_arc c old_major =
       (Printf.sprintf "overtime inf=%f sup=%f" angle_inf angle_sup); *)
   let arc =
     Canvas.create_arc c.widget
-      (Pixels base_x) (Pixels base_y) 
+      (Pixels base_x) (Pixels base_y)
       (Pixels (base_x + d)) (Pixels (base_y + d))
       [ Start angle_inf; Extent angle_sup;
 	FillColor (NamedColor !color_overtime);
 	ArcStyle PieSlice; Outline (NamedColor !color_overtime)]
   in
-  ()
+  ();;
 
 
 let create_secteurs clock times =
@@ -230,14 +232,15 @@ let create_secteurs clock times =
       let inf = if !reverse then -. !total_time +. acc else -. acc in
       let angle_inf = (inf /. !base_major) *. angle_base +. 90.0 in
       let new_acc = acc +. t in
-      if !total_time < (-. new_acc) then 
+      if !total_time < (-. new_acc) then
         (prerr_endline
            "The sum of the given times is greater than the total time.";
          [])
       else
         (
          let angle_sup_pre = (-. t /. !base_major) *. angle_base in
-         let angle_sup = if !reverse then -. angle_sup_pre else angle_sup_pre in
+         let angle_sup =
+           if !reverse then -. angle_sup_pre else angle_sup_pre in
          (*Printf.printf "overtime inf=%f sup%f new_acc=%f"
                        angle_inf angle_sup new_acc;  print_newline ();*)
          let arc =
@@ -263,7 +266,7 @@ let last_time = ref (Unix.time ());;
 let rec set_time freq c () =
   let new_time = Unix.time () in
   let gain = (new_time -. !last_time) *. 1000.0 in
-  c.minor_elapsed <- 
+  c.minor_elapsed <-
     (if !reverse then c.minor_elapsed -. gain else c.minor_elapsed +. gain);
   let old_major = c.major_elapsed in
   c.major_elapsed <-
