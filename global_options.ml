34a9a0eb109029473da3e8bef7734e8f0c6be954
weis <>
2003-10-03 09:49:51 +0000

    Divide and conquer: Active-DVI options are in Global_options, i.e. out of the general purpose module Options.

---
+++ global_options.ml
@@ -0,0 +1,54 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                             Active-DVI                              *)
+(*                                                                     *)
+(*                   Projet Cristal, INRIA Rocquencourt                *)
+(*                                                                     *)
+(*  Copyright 2003 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the GNU Lesser General Public License.          *)
+(*                                                                     *)
+(*  Pierre Weis.                                                       *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* Global Active-DVI options. *)
+
+(* To print debugging messages. *)
+let debug_endline = Options.debug "--debug" " General debug";;
+
+Misc.forward_debug_endline := debug_endline;;
+
+(* Some global options *)
+
+let pson =
+  if Config.have_gs then
+    Options.flag true
+      "-nogs"
+      " Turn off the display of inlined Postscript.\
+      \n\t (the default is to display inlined Postscript)."
+  else ref false;;
+
+let get_global_display_mode, set_global_display_mode =
+ let global_display_mode = ref true in
+ (fun () -> !global_display_mode),
+ (fun b ->
+    GraphicsY11.set_global_display_mode b;
+    global_display_mode := b);;
+
+Options.add "-fg"
+ (Arg.Unit (fun () -> set_global_display_mode false))
+ " Set the drawing policy to ``screen only'',\
+ \n\t (the default is to draw both on the screen and in the memory).";;
+
+Options.add "-w"
+ (Arg.String
+    (function
+     | "a" -> Misc.set_warnings false
+     | "A" -> Misc.set_warnings true
+     | s -> raise (Arg.Bad (Printf.sprintf "-w %s is unknown" s))))
+ "<flags>  Enable/disable warnings according to <flags>,\
+ \n\t A/a enable/disable all warnings\
+ \n\t (the default is \"A\", to enable all warnings).";;
