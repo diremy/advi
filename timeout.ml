769f2689d86fdfbe9e4f1540279a1f98e1134451
weis <>
2004-08-27 21:10:25 +0000

    Review.

--- timeout.ml
+++ timeout.ml
@@ -26,8 +26,7 @@ module Timeout = struct
       stamp : int;
     }
   let compare t t' = compare t.at t'.at
-end
-;;
+end;;
 
 module TimeoutSet = Set.Make(Timeout);;
 
@@ -39,7 +38,7 @@ let set = ref TimeoutSet.empty;;
 let stamp = ref 0;;
 
 let callback () =
-  let rec callback() = 
+  let rec callback () =
     let min = TimeoutSet.min_elt !set in
     let now = Unix.gettimeofday () in
     let wait = min.at -. now in
@@ -48,22 +47,20 @@ let callback () =
       begin
         set := TimeoutSet.remove min !set;
         min.callback ();
-        callback()
+        callback ()
       end
-    else 
+    else
       ignore
         (Unix.setitimer Unix.ITIMER_REAL
            {Unix.it_interval = 0.0; Unix.it_value = wait}) in
-  try callback() with Not_found -> ()
-;;
+  try callback () with Not_found -> ();;
 
 let init () =
   (* Already existing itimer's handler is disabled! *)
   ignore (Sys.signal Sys.sigalrm
             (Sys.Signal_handle (fun _ -> callback ())));
   set := TimeoutSet.empty;
-  stamp := 0
-;;
+  stamp := 0;;
 
 let add sec cbk =
   if sec < 0.001 then raise (Invalid_argument "Timeout.add");
@@ -74,20 +71,17 @@ let add sec cbk =
   let timeout = { at = at; callback = cbk; stamp = mystamp} in
   set := TimeoutSet.add timeout !set;
   callback ();
-  timeout
-;;
+  timeout;;
 
 let rec repeat sec cbk =
   ignore
     (add sec
        (fun () ->
           try cbk (); repeat sec cbk with
-          | exn -> repeat sec cbk; raise exn))
-;;
+          | exn -> repeat sec cbk; raise exn));;
 
 let remove timeout =
   if not (TimeoutSet.mem timeout !set) then raise Not_found;
   set := TimeoutSet.remove timeout !set;
-  callback ()
-;;
+  callback ();;
 
