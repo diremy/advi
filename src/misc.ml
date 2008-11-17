c49b162e8b514a24d5e66dee7d009f40d6d088d2
weis <>
2008-11-17 08:58:56 +0000

    Typos.

--- src/misc.ml
+++ src/misc.ml
@@ -38,17 +38,20 @@ let handle_fatal_error f () =
 (* To emit a warning. *)
 let emit_warning mes =
   Printf.fprintf stderr "Warning: %s" mes;
-  prerr_newline ();;
+  prerr_newline ()
+;;
 
 (* To set up and output warnings. *)
 let set_warnings, warning =
   let warnings = ref true in
   (fun b -> warnings := b),
-  (fun s -> if !warnings then emit_warning s);;
+  (fun s -> if !warnings then emit_warning s)
+;;
 
 (* Debugging. *)
 let forward_debug_endline =
-  ref (function (_ : string) -> failwith "undefined forward debug_endline");;
+  ref (function (_ : string) -> failwith "undefined forward debug_endline")
+;;
 
 let debug_endline s = (!forward_debug_endline s : unit);;
 
@@ -60,28 +63,34 @@ let debug_stop s =
    flush stderr;
    let _ = input_line stdin in
    prerr_endline "Ok"
-  end;;
+  end
+;;
 
 (* Pushing events in the advi's events queue. *)
 let forward_push_char_event =
   ref (fun (c : char) ->
-         failwith "undefined forward push_char_event");;
+         failwith "undefined forward push_char_event")
+;;
 let forward_push_key_event =
   ref (fun (c : char) (ms : modifiers) ->
-         failwith "undefined forward push_key_event");;
+         failwith "undefined forward push_key_event")
+;;
 let forward_push_mouse_event =
   ref (fun (x : mouse_x) (y : mouse_y) (b : button_pressed) ->
-         failwith "undefined forward push_mouse_event");;
+         failwith "undefined forward push_mouse_event")
+;;
 let forward_push_full_event =
   ref (fun (c : char) (ms : modifiers) (k : key_pressed)
            (x : mouse_x) (y : mouse_y) (b : button_pressed) ->
-         failwith "undefined forward push_full_event");;
+         failwith "undefined forward push_full_event")
+;;
 
 let push_char_event c = (!forward_push_char_event c : unit);;
 let push_key_event c ms = (!forward_push_key_event c ms : unit);;
 let push_mouse_event x y b = (!forward_push_mouse_event x y b : unit);;
 let push_full_event c ms k x y b =
-  (!forward_push_full_event c ms k x y b : unit);;
+  (!forward_push_full_event c ms k x y b : unit)
+;;
 
 let set_forward_push_char_event f = forward_push_char_event := f;;
 let set_forward_push_key_event f = forward_push_key_event := f;;
@@ -99,28 +108,33 @@ let reverse_filter f l =
   let rec filter res = function
     | [] -> res
     | a :: l -> if f a then filter (a :: res) l else filter res l in
-  filter [] l;;
+  filter [] l
+;;
 
 let reverse_map f l =
   let rec map res = function
     | [] -> res
     | a :: l -> map (f a :: res) l in
-  map [] l;;
+  map [] l
+;;
 
 (* Concat of List-1 and List-2 is  2-tsiL :: List-1 *)
 let rec reverse_concat l1 = function
   | [] -> l1
-  | a :: b -> reverse_concat (a :: l1) b;;
+  | a :: b -> reverse_concat (a :: l1) b
+;;
 
-(* Strings auxilliaries. *)
+(* Strings auxiliaries. *)
 let string_prefix char s =
  let i = String.index s char in
- String.sub s 0 (i + 1);;
+ String.sub s 0 (i + 1)
+;;
 
 let string_suffix char s =
  let l = String.length s in
  let i = String.rindex s char in
- String.sub s i (l - i);;
+ String.sub s i (l - i)
+;;
 
 let filename_extension = string_suffix '.';;
 
@@ -133,18 +147,21 @@ let contains_string_from s i p =
  let rec has i j =
    j >= lp ||
    i < ls && s.[i] = p.[j] && has (i + 1) (j + 1) in
- i >= 0 && has i 0;;
+ i >= 0 && has i 0
+;;
 
 let contains_string s p =
  let ls = String.length s in
  let rec contains i =
    i < ls &&
    (contains_string_from s i p || contains (i + 1)) in
- contains 0;;
+ contains 0
+;;
 
 let has_prefix pre s = contains_string_from s 0 pre;;
 let has_suffix suf s =
-  contains_string_from s (String.length s - String.length suf) suf;;
+  contains_string_from s (String.length s - String.length suf) suf
+;;
 
 exception Match;;
 
@@ -152,7 +169,8 @@ let get_suffix pre str =
   let lpre = String.length pre in
   let lstr = String.length str in
   if has_prefix pre str then String.sub str lpre (lstr - lpre)
-  else raise Match;;
+  else raise Match
+;;
 
 let rec split_string s p start =
   let len = String.length s
@@ -163,17 +181,20 @@ let rec split_string s p start =
     while !i < len && not (p s.[!i]) do incr i done;
     let i1 = !i in
     String.sub s i0 (i1 - i0) :: split_string s p i1
-  end;;
+  end
+;;
 
 let zap_to_char c s =
   let len = String.length s
   and i = ref 0 in
   while !i < len && s.[!i] <> c do incr i done;
   let i0 = !i + 1 in
-  if i0 >= len then "" else String.sub s i0 (len - i0);;
+  if i0 >= len then "" else String.sub s i0 (len - i0)
+;;
 
 let int_of_float_of_string s =
-  try int_of_string s with _ -> truncate (float_of_string s);;
+  try int_of_string s with _ -> truncate (float_of_string s)
+;;
 
 let is_digit c = c >= '0' && c <= '9';;
 
@@ -191,7 +212,8 @@ let string_substitute_var f s =
         loop ib)
     | c -> Buffer.add_char b c; loop ib) in
   try loop (Scanf.Scanning.from_string s) with
-  | _ -> Buffer.contents b;;
+  | _ -> Buffer.contents b
+;;
 
 let string_replace pat templ str =
   let result = Buffer.create (String.length str * 2) in
@@ -212,7 +234,8 @@ let string_replace pat templ str =
         Buffer.add_string result
           (String.sub str pos (String.length str - pos));
         Buffer.contents result in
-  replace 0;;
+  replace 0
+;;
 
 (* lift a function to an option type *)
 let lift f a = match a with None -> () | Some v -> f v;;
