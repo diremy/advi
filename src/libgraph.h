4a549e0414bc48388398173f96be4cc73c67262d
Didier Rémy <didier.remy@inria.fr>
2021-03-15 16:44:40 +0100

    Taking libgraph.h from 4.11.

--- src/libgraph.h
+++ src/libgraph.h
@@ -1,22 +1,30 @@
-/***********************************************************************/
-/*                                                                     */
-/*                           Objective Caml                            */
-/*                                                                     */
-/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
-/*                                                                     */
-/*  Copyright 1996 Institut National de Recherche en Informatique et   */
-/*  en Automatique.  All rights reserved.  This file is distributed    */
-/*  under the terms of the GNU Library General Public License, with    */
-/*  the special exception on linking described in file ../../LICENSE.  */
-/*                                                                     */
-/***********************************************************************/
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           */
+/*                                                                        */
+/*   Copyright 1996 Institut National de Recherche en Informatique et     */
+/*     en Automatique.                                                    */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
 
-/* $Id$ */
+/**************************************************************************
+/*   This file is copied from the Graphics package distribution: 
+
+https://raw.githubusercontent.com/ocaml/graphics/master/src/unix/libgraph.h 
+
+**************************************************************************/
 
 #include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <caml/mlvalues.h>
+#include <caml/misc.h>
 
 struct canvas {
   int w, h;                     /* Dimensions of the drawable */
@@ -30,12 +38,12 @@ extern Colormap caml_gr_colormap;     /* The color map */
 extern struct canvas caml_gr_window;  /* The graphics window */
 extern struct canvas caml_gr_bstore;  /* The pixmap used for backing store */
 extern int caml_gr_white, caml_gr_black;    /* Black and white pixels for X */
-extern int caml_gr_background;        /* Background color for X 
+extern int caml_gr_background;        /* Background color for X
                                      (used for CAML color -1) */
 extern Bool caml_gr_display_modeflag;     /* Display-mode flag */
 extern Bool caml_gr_remember_modeflag;    /* Remember-mode flag */
-extern int caml_gr_x, caml_gr_y;            /* Coordinates of the current point */
-extern int caml_gr_color;             /* Current *CAML* drawing color (can be -1) */
+extern int caml_gr_x, caml_gr_y;      /* Coordinates of the current point */
+extern int caml_gr_color;        /* Current *CAML* drawing color (can be -1) */
 extern XFontStruct * caml_gr_font;    /* Current font */
 extern long caml_gr_selected_events;  /* Events we are interested in */
 extern Bool caml_gr_ignore_sigio;     /* Whether to consume events on sigio */
@@ -55,7 +63,7 @@ extern int caml_gr_bits_per_pixel;
 #define DEFAULT_SCREEN_WIDTH 600
 #define DEFAULT_SCREEN_HEIGHT 450
 #define BORDER_WIDTH 2
-#define DEFAULT_WINDOW_NAME "Caml graphics"
+#define DEFAULT_WINDOW_NAME "OCaml graphics"
 #define DEFAULT_SELECTED_EVENTS \
             (ExposureMask | KeyPressMask | StructureNotifyMask)
 #define DEFAULT_FONT "fixed"
@@ -75,7 +83,10 @@ extern int caml_gr_bits_per_pixel;
 #endif
 #endif
 
-extern void caml_gr_fail(char *fmt, char *arg);
+CAMLnoreturn_start
+extern void caml_gr_fail(const char *fmt, const char *arg)
+CAMLnoreturn_end;
+
 extern void caml_gr_check_open(void);
 extern unsigned long caml_gr_pixel_rgb(int rgb);
 extern int caml_gr_rgb_pixel(long unsigned int pixel);
